09dcc913503c78aa23f5f6eade644820aae3ea29
LenkaIaco <117647840+LenkaIaco@users.noreply.github.com>
2022-11-24 13:47:26 +0100

    updated annotations/files to OOP topic

--- git/java1/java1/src/main/java/sk/itlearning/java1/g/zakaznik/Obec.java
+++ git/java1/java1/src/main/java/sk/itlearning/java1/g/zakaznik/Obec.java
@@ -1,6 +1,6 @@
 package sk.itlearning.java1.g.zakaznik;
 
-public class Obec implements Zakaznik {
+public class Obec implements Zakaznik { // nadtrieda Zakaznik je abstraktna, tj nedodefinovana. vsetky podtriedy musia byt abstraktne. bud ju zadefinujem tu ako abstraktnu alebo cez override(nechcem mat abstraktnu obec)
 
 	@Override
 	public String getId() {
--- git/java1/java1/src/main/java/sk/itlearning/java1/g/zakaznik/Zakaznik.java
+++ git/java1/java1/src/main/java/sk/itlearning/java1/g/zakaznik/Zakaznik.java
@@ -1,7 +1,8 @@
 package sk.itlearning.java1.g.zakaznik;
 
-public interface Zakaznik {
-
-	String getId();
+public abstract  Zakaznik { //miesto interface dali sme abstract
+	//tato trieda nema funkcionalitu iba definuje pravidla(podtriedy riadi).Priklad z praxe: Web browser a www komunikuju cez http protokol
+// je to interface na vymenu medzi inputmi a nasimi zadefinovanymi kodovymi strukturami
+	public abstract String getId();//ak sa v triede nachadza aspon 1 absttr metoda tak cela trieda musi byt abstraktna
 	
 }
